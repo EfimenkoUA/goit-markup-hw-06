87e6658ce0b9c2ec5884e92aa27ad6b60921f8fe
Yuriy <uae.zpr@gmail.com>
2023-10-05 22:19:11 +0300

    udate

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 
-    (() => {
+    /*(() => {
         const refs = {
             openPopupBtn: document.querySelector('[data-popup-open]'),
             closePopupBtn: document.querySelector('[data-popup-close]'),
@@ -12,4 +12,24 @@
         function togglePopup() {
             refs.popup.classList.toggle('is-hidden');
         }
-    })();
+    })();*/
+
+    // Меню бургер
+    const iconMenu = document.querySelector('.menu-toggle');
+    const menuBody = document.querySelector('.menu-container');
+    const closeMenu = document.querySelector('.mobile-menu-btn');
+if (iconMenu) {
+    iconMenu.addEventListener("click", function (e) {
+        //document.body.classList.toggle('_lock');
+        iconMenu.classList.toggle('is-open');
+        menuBody.classList.toggle('is-open');
+    });
+}
+
+if (closeMenu) {
+    closeMenu.addEventListener("click", function (e) {
+        //document.body.classList.toggle('_lock');
+        console.log('ok')
+        menuBody.classList.remove('is-open');
+    });
+}
